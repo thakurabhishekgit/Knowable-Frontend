e5796160ffc73d4f8dff20a281e462c2c5e5babb
thakur Abhishek Singh <thakur.abhisheksinght97@gmail.com>
2025-07-14 17:05:59 +0000

    i think yolu have fixed the navbar abd footer from main css file alsovnh

--- src/components/app-layout.jsx
+++ src/components/app-layout.jsx
@@ -51,10 +51,7 @@ export function AppLayout({ children }) {
   const isDocumentPage = pathname.startsWith('/document/');
 
   return (
-    <div className={cn(
-        "flex flex-col",
-        isDocumentPage ? "h-screen" : "min-h-screen"
-    )}>
+    <div className="flex flex-col h-full">
       <header className="flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 sticky top-0 z-50 shrink-0">
         
         {/* Mobile Menu */}
@@ -121,7 +118,7 @@ export function AppLayout({ children }) {
             <UserNav />
         </div>
       </header>
-      <main className="flex-1 min-h-0">
+      <main className="flex-1 flex flex-col min-h-0">
         {children}
       </main>
       {!isDocumentPage && <Footer />}
