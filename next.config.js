87dc910bc3b5875f55d3f417de79caa868e95f1c
thakur Abhishek Singh <thakur.abhisheksinght97@gmail.com>
2025-07-14 10:22:47 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`, // Proxy to Backend
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`, // Proxy to Backend
       },
     ]
   },
